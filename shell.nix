a36137a2b9675f5e9b7e0a7840bc9fe9f2414d4e
adisbladis <adisbladis@gmail.com>
2018-10-14 20:35:05 +0800

    Remove stop env propagation hack

--- shell.nix
+++ shell.nix
@@ -1,23 +1,14 @@
 with (import <nixpkgs> {});
 
-let
-
-  # Expose all bin outputs from `go` without
-  # propagating their env vars
-  #
-  # This makes sure we can use gofmt with vgo
-  goNoPropagation = runCommand "go-no-propagation" {} ''
-    mkdir -p $out/bin
-    for f in ${go}/bin/*; do
-      ln -sf $f $out/bin/$(basename $f)
-    done
-  '';
-
-in mkShell rec {
+mkShell rec {
   buildInputs = [
     nix-prefetch-git
-    goNoPropagation
+    go
   ] ++ stdenv.lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.Security
   ];
+
+  shellHook = ''
+    unset GOPATH
+  '';
 }
