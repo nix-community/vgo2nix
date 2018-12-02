b298f4fb799fc532488fc887e1938668d7f3d219
adisbladis <adisbladis@gmail.com>
2018-12-02 17:46:57 +0000

    Fix nix expression to contain go compiler references

--- default.nix
+++ default.nix
@@ -1,22 +1,30 @@
-{ stdenv, lib, buildGoPackage, go, makeWrapper, nix-prefetch-git }:
+with import <nixpkgs> {};
 
 assert lib.versionAtLeast go.version "1.11";
 buildGoPackage rec {
   name = "vgo2nix-${version}";
-  version = "0.0.1";
+  version = "unstable-2018-10-14";
   goPackagePath = "github.com/adisbladis/vgo2nix";
 
   nativeBuildInputs = [ makeWrapper ];
 
-  src = ./.;
+  src = lib.cleanSource ./.;
+
   goDeps = ./deps.nix;
 
-  CGO_ENABLED = 0;
+  allowGoReference = true;
 
   postInstall = with stdenv; let
     binPath = lib.makeBinPath [ nix-prefetch-git go ];
   in ''
     wrapProgram $bin/bin/vgo2nix --prefix PATH : ${binPath}
   '';
+
+  meta = with stdenv.lib; {
+    description = "Convert go.mod files to nixpkgs buildGoPackage compatible deps.nix files";
+    homepage = https://github.com/adisbladis/vgo2nix;
+    license = licenses.mit;
+    maintainers = with maintainers; [ adisbladis ];
+  };
 
 }
