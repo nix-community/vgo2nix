183a22a6f50aacaeb9dfae13a17160ea52e800f5
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2019-11-19 00:31:00 +0200

    Handle module replacements

--- main.go
+++ main.go
@@ -63,10 +63,15 @@ func getModules() ([]*modEntry, error) {
 		return nil, err
 	}
 
+	type goModReplacement struct {
+		Version string
+	}
+
 	type goMod struct {
 		Path    string
 		Main    bool
 		Version string
+		Replace *goModReplacement
 	}
 
 	var mods []goMod
@@ -77,6 +82,10 @@ func getModules() ([]*modEntry, error) {
 			break
 		} else if err != nil {
 			return nil, err
+		}
+
+		if mod.Replace != nil {
+			mod.Version = mod.Replace.Version
 		}
 
 		if !mod.Main {
