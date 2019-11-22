2e372533599fe16337331b3c4b0f66b3e2d10cef
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2019-11-22 08:31:56 +0200

    Add version replacement test

---
+++ tests/test_replace_version/expected.nix
@@ -0,0 +1,21 @@
+# file generated from go.mod using vgo2nix (https://github.com/adisbladis/vgo2nix)
+[
+  {
+    goPackagePath = "github.com/coreos/go-systemd";
+    fetch = {
+      type = "git";
+      url = "https://github.com/coreos/go-systemd";
+      rev = "v22.0.0";
+      sha256 = "0p4sb2fxxm2j1xny2l4fkq4kwj74plvh600gih8nyniqzannhrdx";
+    };
+  }
+  {
+    goPackagePath = "github.com/godbus/dbus";
+    fetch = {
+      type = "git";
+      url = "https://github.com/godbus/dbus";
+      rev = "v5.0.3";
+      sha256 = "1bkc904073k807yxg6mvqaxrr6ammmhginr9p54jfb55mz3hfw3s";
+    };
+  }
+]
